c74ecee1a1a6f0aec81b62f6d15a810cf46364e7
iromashko <iromashko@me.com>
2020-11-03 21:43:49 +0300

    part 1

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { LessonModule } from './lesson/lesson.module';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
       autoSchemaFile: true,
+    }),
+    LessonModule,
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb://localhost/school',
+      synchronize: true,
+      useUnifiedTopology: true,
+      autoLoadEntities: true,
     }),
   ],
   controllers: [],
