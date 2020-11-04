c44d50aa22f159373b9c25f9bf59eeb99640452f
iromashko <iromashko@me.com>
2020-11-04 03:50:47 +0300

    part 4

--- src/lesson/lesson.service.ts
+++ src/lesson/lesson.service.ts
@@ -29,4 +29,13 @@ export class LessonService {
   async getLessons(): Promise<Lesson[]> {
     return this.lessonRepository.find();
   }
+
+  async assignStudentsToLesson(
+    lessonId: string,
+    studentIds: string[],
+  ): Promise<Lesson> {
+    const lesson = await this.lessonRepository.findOne({ id: lessonId });
+    lesson.students = [...lesson.students, ...studentIds];
+    return this.lessonRepository.save(lesson);
+  }
 }
